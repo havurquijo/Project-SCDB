4df5a46504e8ed5adab084098c5f560d5ee28cb0
havurquijo <158757897+havurquijo@users.noreply.github.com>
2024-06-02 13:45:39 -0300

    update in license

--- flask_app/SCDB_ML_app.py
+++ flask_app/SCDB_ML_app.py
@@ -1,3 +1,20 @@
+'''
+SCDB-ML-app is a deployed app to analize the U.S. Supreme Court Database
+Copyright (C) 2024  HERMES A. V. URQUIJO
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 from flask import Flask,redirect,url_for,render_template,request,jsonify,send_file # type: ignore
 from mine_decision_tree import mine_decision_tree as mine_tree_class
 from predict_decision_tree import predict_decision_tree as predict_tree_class
--- flask_app/static/js/mining_tree.js
+++ flask_app/static/js/mining_tree.js
@@ -1,3 +1,20 @@
+/*
+SCDB-ML-app is a deployed app to analize the U.S. Supreme Court Database
+Copyright (C) 2024  HERMES A. V. URQUIJO
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('myForm_mining');
     const toastTrainedTree = document.getElementById('liveToast_mining');
